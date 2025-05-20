3c3f83c0bffaa62660006ece92ee87ffc18101f4
Vlad Mor <vladmorlo@gmail.com>
2025-05-20 19:39:22 -0300

    DB 1

---
+++ Banco de Dados 1/Desenvolvimento 6.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE ESCOLA;
+
+USE ESCOLA;
+
+CREATE TABLE ALUNO (
+    ID INT PRIMARY KEY,
+    Nome VARCHAR(100),
+    Matricula INT,
+    Email VARCHAR(100),
+    Endereco VARCHAR(150),
+    Telefone VARCHAR(20)
+);
